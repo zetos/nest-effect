a580d32ef3f8cbf8fbf4317bec356f4f9f73f8ad
Zetos Lima <cesar_manoel@hotmail.com>
2025-08-30 19:58:11 -0300

    chore: enforce strict type checking in ESLint and TypeScript configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,7 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn'
     },
