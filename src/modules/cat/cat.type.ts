5c0e120c2c5a970000f9e85035e713756c35547a
zetos <cesar_manoel@hotmail.com>
2026-07-19 21:48:45 -0300

    feat: upgrade pkgs
    waking this project from torpor.

--- src/modules/cat/cat.type.ts
+++ src/modules/cat/cat.type.ts
@@ -1,11 +1,8 @@
 import { Schema } from 'effect';
 
-const _Cat = Schema.Struct({
+export const Cat = Schema.Struct({
   id: Schema.String,
   name: Schema.String,
 });
 
-export type Cat = Schema.Schema.Type<typeof _Cat>; // TS type can be use exactly same as before
-// export interface Cat extends Schema.Schema.Type<typeof _Cat> {} // TS type can be use exactly same as before
-
-export const Cat: Schema.Schema<Cat> = _Cat; // Schema type that will be use for decoding
+export type Cat = typeof Cat.Type;
